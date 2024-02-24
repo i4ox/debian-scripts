6bf4b6238dffd3edf54ce9329520024e3257dc15
i4ox <crai-dvps@tuta.io>
2024-02-24 14:36:59 +0300

    add nm

--- load-nm.sh
+++ load-nm.sh
@@ -5,18 +5,19 @@
 [ -x "$(command -v sudo)" ] && ld="sudo"
 
 # Установка NetworkManager
-$ld apt install network-manager wpasupplicant
+$ld apt install network-manager wpasupplicant -yy
 
 # Заменяем конфиг по-умолчанию
 wget https://codeberg.org/i4ox/dotfiles/raw/branch/main/services/NetworkManager.conf
-mv NetworkManager.conf /etc/NetworkManager/NetworkManager.conf
+$ld chmod 644 NetworkManager.conf
+$ld mv NetworkManager.conf /etc/NetworkManager/NetworkManager.conf
 
 # Отключаем networking
-systemctl stop networking
-systemctl disable networking
+$ld systemctl stop networking
+$ld systemctl disable networking
 
 # Включаем Network-Manager
-systemctl enable wpa_supplicant
-systemctl enable NetworkManager
-systemctl restart wpa_supplicant
-systemctl restart NetworkManager
+$ld systemctl enable wpa_supplicant
+$ld systemctl enable NetworkManager
+$ld systemctl restart wpa_supplicant
+$ld systemctl restart NetworkManager
